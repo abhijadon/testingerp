ff93b9129ffefa36ccafa7045f72b6fc24f75d53
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-11-26 23:51:34 +0530

    htis

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,13 +1,13 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8888/api/';
+    : 'https://sdoddsdf.onrender.com/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:8888/';
+    : 'https://sdoddsdf.onrender.com/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:8888/download/';
+    : 'https://sdoddsdf.onrender.com/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
