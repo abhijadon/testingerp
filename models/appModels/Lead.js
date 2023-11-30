b4a2dc68c5c7d8fd9d230abed007e3ab4a9a5103
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-11-30 16:48:49 +0530

    this

--- models/appModels/Lead.js
+++ models/appModels/Lead.js
@@ -10,50 +10,12 @@ const applicationSchema = new mongoose.Schema({
     type: Boolean,
     default: true,
   },
-
-  studentid: {
-    type: String,
-    trim: true,
-    required: true,
-  },
   studentname: {
     type: String,
     trim: true,
     required: true,
   },
-  phone: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-  },
-  coursename: {
-    type: String,
-    trim: true,
-  },
-  university: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  state: {
-    type: String,
-    trim: true,
-  },
-  lms: {
-    type: String,
-    trim: true,
-  },
-  status: {
-    type: String,
-    default: 'new',
-  },
+
   created: {
     type: Date,
     default: Date.now,
