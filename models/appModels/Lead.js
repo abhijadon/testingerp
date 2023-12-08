4b96946dca73d0ea48048c27df2c85b0b6a7b5a5
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-12-08 11:25:28 +0530

    \bhish

--- models/appModels/Lead.js
+++ models/appModels/Lead.js
@@ -1,149 +1,154 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const applicationSchema = new mongoose.Schema({
-  removed: {
-    type: Boolean,
-    default: false,
-  },
-  enabled: {
-    type: Boolean,
-    default: true,
-  },
-
-  lead_id: {
-    type: String,
-    trim: true,
-  },
-
-  full_name: {
-    type: String,
-    trim: true,
-  },
-
-  contact: {
-    email: {
-      type: String,
-      trim: true,
-      unique: true,
-      lowercase: true,
+const applicationSchema = new mongoose.Schema(
+  {
+    removed: {
+      type: Boolean,
+      default: false,
     },
-    phone: {
-      type: Number,
-      trim: true,
+    enabled: {
+      type: Boolean,
+      default: true,
     },
-    alternate_phone: {
-      type: Number,
-      trim: true,
-    },
-  },
 
-  education: {
-    course: {
+    lead_id: {
       type: String,
       trim: true,
     },
-    institute: {
-      type: String,
-      trim: true,
-    },
-    specialization: {
-      type: String,
-      trim: true,
-    },
-  },
 
-  customfields: {
-    institute: {
+    full_name: {
       type: String,
       trim: true,
+    },
+
+    contact: {
+      email: {
+        type: String,
+        trim: true,
+        unique: true,
+        lowercase: true,
+      },
+      phone: {
+        type: Number,
+        trim: true,
+      },
+      alternate_phone: {
+        type: Number,
+        trim: true,
+      },
+    },
+
+    education: {
+      course: {
+        type: String,
+        trim: true,
+      },
+      institute: {
+        type: String,
+        trim: true,
+      },
+      specialization: {
+        type: String,
+        trim: true,
+      },
+    },
+
+    customfields: {
+      institute: {
+        type: String,
+        trim: true,
+      },
+      university_name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      sendfeereceipt: {
+        type: String,
+        trim: true,
+      },
+      father_name: {
+        type: String,
+        trim: true,
+      },
+      mother_name: {
+        type: String,
+        trim: true,
+      },
+      session: {
+        type: String,
+        trim: true,
+      },
+      sessionType: {
+        type: String,
+        trim: true,
+      },
+      dob: {
+        type: Date,
+        trim: true,
+        default: null,
+      },
+      remark: {
+        type: String,
+        default: null,
+        trim: true,
+      },
+
+      gender: {
+        type: String,
+        trim: true,
+      },
+
+      installmentType: {
+        type: String,
+        trim: true,
+      },
+
+      paymentMode: {
+        type: String,
+        trim: true,
+      },
+
+      totalPaidAmount: {
+        type: String,
+        trim: true,
+      },
+
+      paidAmount: {
+        type: String,
+        trim: true,
+      },
+
+      duefeeAmount: {
+        type: String,
+        trim: true,
+      },
+
+      counselorEmail: {
+        type: String,
+        trim: true,
+        lowercase: true,
+      },
+    },
+    status: {
+      type: String,
+      default: 'new',
     },
     university: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    sendfeereceipt: {
+      // Define the 'university' field in the schema
       type: String,
       trim: true,
     },
-    fatherName: {
-      type: String,
-      trim: true,
-    },
-    motherName: {
-      type: String,
-      trim: true,
-    },
-    session: {
-      type: String,
-      trim: true,
-    },
-    sessionType: {
-      type: String,
-      trim: true,
-    },
-    dob: {
+    created: {
       type: Date,
-      trim: true,
-      default: null,
-    },
-    remark: {
-      type: String,
-      default: null,
-      trim: true,
-    },
-
-    gender: {
-      type: String,
-      trim: true,
-    },
-
-    installmentType: {
-      type: String,
-      trim: true,
-    },
-
-    paymentMode: {
-      type: String,
-      trim: true,
-    },
-
-    totalPaidAmount: {
-      type: String,
-      trim: true,
-    },
-
-    paidAmount: {
-      type: String,
-      trim: true,
-    },
-
-    duefeeAmount: {
-      type: String,
-      trim: true,
-    },
-
-    counselorEmail: {
-      type: String,
-      trim: true,
-      lowercase: true,
+      default: Date.now,
     },
   },
-  status: {
-    type: String,
-    default: 'new',
-  },
-  university: {
-    // Define the 'university' field in the schema
-    type: String,
-    trim: true,
-  },
-  created: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    strict: false,
+  }
+);
 
 // Middleware to handle dynamic validation based on university selection
 applicationSchema.pre('save', function (next) {
